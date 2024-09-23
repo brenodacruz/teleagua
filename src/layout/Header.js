48ec74d8bce64e87a15a8919f3b06ed1c26451a8
brenodacruz <brenohenriquecruz22@gmail.com>
2024-09-23 17:52:26 -0300

    Pagina de Produtos Finalizada (Front end)

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,20 +4,22 @@ import { useLocation } from 'react-router-dom';
 
 export default function Header(props){
     const location = useLocation()
+    var quantidade = 2
     return(
         <div className="fixed top-0 pl-[300px] left-0 w-screen bg-blue-600 text-white py-5 pr-20 flex flex-row justify-between">
             <section>
                 <h1 className="text-lg">
-                    {location.pathname == '/' ? 'Página Principal' : ''}
-                    {location.pathname == '/produtos' ? 'Produtos' : ''}
-                    {location.pathname == '/clientes' ? 'cliente' : ''}
-                    {location.pathname == '/pagamento' ? 'pagamento' : ''}
-                    {location.pathname == '/finalizar' ? 'finalizar' : ''}
+                    {location.pathname === '/' ? 'Página Principal' : ''}
+                    {location.pathname === '/produtos' ? 'Produtos' : ''}
+                    {location.pathname === '/clientes' ? 'cliente' : ''}
+                    {location.pathname === '/pagamento' ? 'pagamento' : ''}
+                    {location.pathname === '/finalizar' ? 'finalizar' : ''}
                 </h1>
             </section>
-            <section className='flex flex-row gap-5 justify-center items-center'>
+            <section className='flex flex-row gap-1 justify-center items-center'>
+                <h1>{location.pathname === '/produtos' ? 'Carrinho' : ''}</h1>
+                <p className='mr-5'>{location.pathname === '/produtos' ? `(${quantidade})` : ''}</p>
                 <FontAwesomeIcon icon={location.pathname == '/produtos' ? faShoppingCart : ''} />
-                <h1>{props.text2}</h1>
             </section>
         </div>
     )
--- src/pages/Produtos.js
+++ src/pages/Produtos.js
@@ -1,7 +1,25 @@
+import ListaProdutos from "../components/ListaProdutos";
+import ProdutosSelecionados from "../components/ProdutosSelecionados";
+
 export default function Produtos(){
     return(
-        <div className="flex h-screen w-screen justify-center items-center">
-            <h1>Produtos</h1>
+        <div className="grid grid-cols-[1fr_auto] h-screen w-screen pl-[270px] pt-[68px]">
+            <section className="flex flex-col items-center justify-start border-r-2 border-black">
+                <ListaProdutos texto="Água - Porta" valor="15,00"/>
+                <ListaProdutos texto="Água - Perto" valor="18,00"/>
+                <ListaProdutos texto="Água - Longe" valor="19,00"/>
+                <ListaProdutos texto="Galão 20L" valor="24,00"/>
+
+            </section>
+            <section className="flex flex-col items-center justify-start">
+                <h1 className="text-4xl mt-10">Selecionados:</h1>
+                <ProdutosSelecionados produto="Água - Porta" valor="15,00" quantidade="2"/>
+                <ProdutosSelecionados produto="Galão 20L" valor="24,00" quantidade="2"/>
+                
+                <h1 className="text-4xl mt-10">Total: R$ 00,00</h1>
+
+                <img src="../img/giff-galao.gif"></img>
+            </section>
         </div>
     )
 }
