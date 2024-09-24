79b7138623716b9ed17ebff17366ebb7203b50d9
brenodacruz <brenohenriquecruz22@gmail.com>
2024-09-24 14:57:45 -0300

    Pagina Finalizar Front-End

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,20 +6,20 @@ export default function Header(props){
     const location = useLocation()
     var quantidade = 2
     return(
-        <div className="fixed top-0 pl-[300px] left-0 w-screen bg-blue-600 text-white py-5 pr-20 flex flex-row justify-between">
+        <div className="fixed top-0 pl-[300px] left-0 w-screen bg-blue-600 text-white py-5 pr-32 flex flex-row justify-between">
             <section>
                 <h1 className="text-lg">
                     {location.pathname === '/' ? 'Página Principal' : ''}
                     {location.pathname === '/produtos' ? 'Produtos' : ''}
-                    {location.pathname === '/clientes' ? 'cliente' : ''}
-                    {location.pathname === '/pagamento' ? 'pagamento' : ''}
-                    {location.pathname === '/finalizar' ? 'finalizar' : ''}
+                    {location.pathname === '/clientes' ? 'Lista de Clientes' : ''}
+                    {location.pathname === '/pagamento' ? 'Formas de Pagamento' : ''}
+                    {location.pathname === '/finalizar' ? 'Finalizar' : ''}
                 </h1>
             </section>
             <section className='flex flex-row gap-1 justify-center items-center'>
                 <h1>{location.pathname === '/produtos' ? 'Carrinho' : ''}</h1>
                 <p className='mr-5'>{location.pathname === '/produtos' ? `(${quantidade})` : ''}</p>
-                <FontAwesomeIcon icon={location.pathname == '/produtos' ? faShoppingCart : ''} />
+                <FontAwesomeIcon icon={location.pathname === '/produtos' ? faShoppingCart : ''} />
             </section>
         </div>
     )
